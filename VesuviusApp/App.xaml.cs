6b1aafc5d91e6a8afd94c4194a06b9897cd8c724
Sebastian Slangerup Møller <sebastian.slangerup1@gmail.com>
2023-05-30 12:34:15 +0200

    Initial commit

---
+++ VesuviusApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VesuviusApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ VesuviusApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VesuviusApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
